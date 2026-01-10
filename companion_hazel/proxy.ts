00f1e606f0192406e0e9ee23a7aa9cd6e51b63ec
Onitha <onithahettiarachchi26@gmail.com>
2026-01-10 16:46:09 +0530

    Package-lock duplication removed

--- companion_hazel/proxy.ts
+++ companion_hazel/proxy.ts
@@ -1,12 +1,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   return NextResponse.next()
 }
-
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: '/:path*',
 }
